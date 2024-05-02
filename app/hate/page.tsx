841438bf539673550b57695340acec5ee598e1c8
Kelly Davis <kldavis4@gmail.com>
2024-05-02 12:28:08 -0400

    add new page

---
+++ app/hate/page.tsx
@@ -0,0 +1,139 @@
+export default async function Hate() {
+
+    return <div>
+        <h1 className="text-lg font-bold">How do you have fun to do something you hate?</h1>
+        <a className="underline" href="https://www.quora.com/How-do-you-have-fun-to-do-something-you-hate">source</a>
+        <div>
+            <h2 className="font-bold">Mess Jenson</h2>
+        You can hardly make something fun when you hate it. You can just make it more acceptable by thinking about the rewards or accepting it.
+
+Like you hate to work but you still goes to work everyday. Maybe you think about gossiping with your colleagues or the great lunch you can have or the happy hours after work. You just think about the good after work, not work itself. (Of course, you’ll still need to think a little about and complete your work)
+
+Unless you’ve complete or a lot of control over something, you can hardly make it fun.
+
+Like exercise, you’ve complete control over how and what you’re going to do. This can be easier to become fun - say you choose to exercise in the form of paintball/war game, have a buddy, treasure hunting…etc.
+        </div>
+        <div>
+            <h2 className="font-bold">Rajan Singh</h2>
+            You cannot have fun unless you enjoy and love it. Give full dedication to the task of your hatred which you're assigned to. And engross yourself with positive thoughts regarding the consequence of your assignment.
+
+For example: I'm currently preparing myself for CAT 2021 (supposing if you don't know what CAT is, it's an entrance examination to study MBA). The syllabus is huge with a total of 3 papers. Instead of getting discouraged by seeing the length of the syllabus, I am rather dreaming of myself to walk along the campus of IIM ABC (my dream colleges) after 2 years, getting the fame, compliments from my relatives and friends. See, I don't know what will happen at the CAT exam. But definitely I'm dreaming of myself to be in a better position compared to my current position. So yes, whenever you're doing a task, never stop dreaming. Dream yourself to be in a better position after completing the task. Think about the satisfaction and confidence you'll get after completing the task. So yes, in this way you'll get plenty of motivation.
+        </div>
+        <div>
+            <h2 className="font-bold">Moisey Uretsky</h2>
+            What this means is that if you continue to look at this situation as challenging, or frustrating, or something to over come, sure you can make progress, but you will be depleting your will power reserve around something that you don't really even want to do.
+
+What I have seen work is reframing the perspective. By changing the perspective slightly you can completely no longer be frustrated by it. I'll give you three examples.
+
+<p>Losing weight: I wanted to lose weight and I knew that mayo was unhealthy. So instead of deciding to deny myself mayo (will power), I decided to try mustard instead. It turned out I greatly enjoyed mustard and so I didn't have to feel that I was denying myself something, but instead just enjoy a great sandwich.</p>
+<p>Making the bed: Ok, not something that I enjoy doing, but there have been numerous studies written that very successful people often make their bed. Now when I do it, I just remind myself that I'm starting my day off right. I'm getting a small to-do item done and want to carry that momentum into the day. Plus, coming home after a long day of work and having the bed made, actually is quite nice. So something that was a bit of a chore became a ritual to remind myself to get into a positive mental attitude before heading for the office.</p>
+<p>Commuting to work: I commuted to high school, college, and work for a total of about 15 years. First by subway (an hour each way), and then by car (an hour each way) the last 5 years. What I discovered is that on the subway I got a chance to read. Which I love. There's no faster way to get things done in life then to read voraciously, because any challenge you face someone has already dealt with, so why not learn from the best how they faced those challenges. When I started driving I could no longer read and I began to miss it. It got to the point, that while the drive was a bit more convenient I would often enough choose the subway to give myself the time to do that. Without the many books I've read there's no way that DigitalOcean would find the success that it has today. So it was a perspective shift of taking a boring commute and using it as something that really helps me be a better person in all capacities, business strategy, product management, leadership, management, buddhism, whatever the topic of interest is for me that week.</p>
+
+Now finding these perspective shifts for particular situations can be challenging, but if you do, you will be significantly less stressed and more confident that you will be able to carry out the results you intended because you aren't fighting an up-hill battle.
+        </div>
+
+        <div>
+            <h2 className="font-bold">Ashley Zang</h2>
+            Thinking about the benefits this thing will bring you. Try to do it seriously for five minutes first. Once you calm down and become sane, you then continue to do the thing.
+
+If you are still irritable after five minutes, then think about whether you are willing to bear the result after failing to complete the goal. If not, try again from the beginning for another five minutes.
+        </div>
+
+        <div>
+            <h2 className="font-bold">Antti Vanhanen</h2>
+            Hello friend,
+
+let’s take a step back and focus on the amount of tension in the question.
+
+Can you notice it?
+
+You don’t want to do anything… and yet you want to motivate yourself to take action.
+
+There is inner conflict there that says:
+
+“I should be different than what I am.”
+
+This is what most of us do - we put pressure on ourselves to feel, think, and behave differently.
+
+And yet, at the same time, we absolute despise the feeling of being forced to change.
+
+In fact, most of us resist the attempts to change more than we actually want to change.
+
+That’s why no matter how much energy you channel into changing or motivating yourself, the part of you that doesn’t want to change will still be stronger.
+
+If the energy doesn’t translate into change, where does it go?
+
+It goes into infighting with yourself, which is the very definition of suffering.
+
+So what’s the alternative?
+
+Instead of trying to change yourself, see how long you can be idle and just sit with your lack of motivation.
+
+Think of what you should do and then just acknowledge that you don’t want to do it.
+
+Just sit there with the feeling.
+
+Don’t allow yourself to get distracted by anything else.
+
+Just sit there.
+
+Hang out with your lack of motivation as long as it wants to hang out with you.
+
+Drink a cup of tea with it.
+
+Listen to what it tells you.
+
+Feel how it feels and where you can feel it.
+
+Really observe it. Study it like you were an anthropologist that just discovered a new species of animal.
+
+What you will inevitably find is the lack of motivation is but a temporary guest that just wants to be seen and heard.
+
+When you do that, at some point, your feelings of resistance will slowly fade and you will discover that an urge to take action seemingly wants to jump out from within you.
+
+What I am hoping you get from this answer is that it is seldom a good idea to force yourself to change.
+
+It just creates more resistance and frustration that depletes your energy reserves.
+
+And in addition to feeling terrible, it usually just doesn’t lead to results.
+
+So instead, why don’t you try being gentle with yourself?
+
+Instead of judging yourself as someone who is lazy or who procrastinates, give yourself the attention that your heart really needs.
+
+Listen to your heart’s insecurities and concerns.
+
+Be attentive to how your heart is really trying to protect you from emotional discomfort by refusing to take action.
+
+Be there for your heart so it can feel what it needs to feel.
+
+Don’t argue with your heart, because the heart doesn’t understand words.
+
+When you create a space for yourself to feel whatever it is that is there to feel, you allow that feeling to be released.
+
+And once it is released, it is no longer there acting as a barrier to action.
+
+Then action flows freely and effortlessly.
+
+This is the gentle way of being human.
+
+I sincerely hope you hear something in these words that allows you to give yourself that gift.
+        </div>
+        <div>
+            <h2 className="font-bold">Kim Ngo</h2>
+            Hm, sounds like high school.
+
+I'm 17, and my school ranges from mildly tolerable to downright insufferable. We have some great teachers, but that doesn't change the fact that the administration runs like a political machine with its sole interest invested in self promotion rather than the actual students.
+
+But I still get up and go to school five days a week. Sometimes it's not about what you're doing but rather why you're doing it. Will going to high school help me get into college? Certainly. Determining a larger goal that legitimizes the necessity of what you're doing, especially if you hate doing it, will help you retain your focus.
+
+Some people love cooking but not washing the dishes, right? But if the dishes are never washed, then they can't cook like they want to.
+
+You have to motivate yourself. Think of it like a stairway. Some steps are going to have stains or gum attached to them, but if you don't use them, you'll never get to the top, where you want to be.
+
+"Why can't I just skip a step then?" Wow, what a smart-aleck. The possibility of falling and failing increases with every step your skip, so if you try to skip doing the things you don't like, you might be keep falling back onto the ground, nowhere near your goal.
+
+I hope whatever you're doing is not mind-numbingly tedious. Best of luck.
+        </div>
+    </div>;
+  }
